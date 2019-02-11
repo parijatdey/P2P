4803ad4d131ffe8d87de58a59b529f7697ac260c
parijatdey <parijatdey@yahoo.com>
2019-02-11 11:32:18 +0530

    Update tests3.py

--- tests3.py
+++ tests3.py
@@ -6,6 +6,6 @@ s3 = boto3.resource('s3')
 # Print out bucket names
 for bucket in s3.buckets.all():
     print(bucket.name)
-    for filelist in s3.Bucket(bucket.name).objects():
-        print(filelist)
+    for filelist in s3.Bucket(bucket.name).objects.page_size(20):
+        print(filelist.key)
  
